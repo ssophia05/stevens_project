2f28f07aec2171527a7d258588e9a9e0bb4dd9e4
ssophia05 <ssophia05@vt.edu>
2026-04-14 13:43:32 -0400

    updating glm script

--- stevens_glm.R
+++ stevens_glm.R
@@ -1,25 +1,38 @@
 library(ggplot2)
 
 swallow_dat = read.csv("5NovPatchData.csv")
-View(data)
+#View(swallow_dat)
 
 # Hypothesis: patch area vs colony size
+# testing to see if larger patch area corresponds with larger colony size
 
-# histogram shows normal distribution
+# histogram of response variable
 hist(swallow_dat$Colony.Size)
 
+# Poisson GLM
 g1 = glm(Colony.Size~Area,data=swallow_dat, family="poisson");
 summary(g1)
 
 library(effects)
 plot(allEffects(g1))
 
+# model shows that patch area is statistically significant predictor of colony size
+# since p-value is much less than 0.05, and since the slope is positive it suggests that
+# larger patch area size relates to larger colony sizes
+
+# plotting
 dat.new = expand.grid(Area=seq(from = min(swallow_dat$Area),
                                to = max(swallow_dat$Area),
                                length.out = 100)
+)
 
-dat.new$yhat  = predict(g2,type="response",newdata = dat.new)
-bat$yhat2 = predict(g2,type="response")
+dat.new$yhat  = predict(g1,type="response",newdata = dat.new)
+swallow_dat$yhat2 = predict(g1,type="response")
 head(dat.new)
+head(swallow_dat)
 
+plot1=ggplot(data=swallow_dat,aes(x=Area,y=Colony.Size))+
+  geom_point(size=2,shape =1) +
+  geom_line(data=dat.new, aes(x=Area,y=yhat))
+plot1
 
